8113ff95f0e1ab4102dfee5ae74e7a3409f46523
bacnguyenvan <bacb4tvk@gmail.com>
2020-04-11 15:58:59 +0700

    update v2

--- do_an_tot_nghiep/admin/api/listVegetable.php
+++ do_an_tot_nghiep/admin/api/listVegetable.php
@@ -2,8 +2,9 @@
     require_once  "../autoload/autoload.php";
     
     
-    $sql = "SELECT * FROM rau WHERE rau.deleted_at='0' ";
-   
+    // $sql = "SELECT * FROM rau WHERE rau.deleted_at='0' ";
+   	$sql = "SELECT rau.*,thu_hoach.nha_san_xuat,thu_hoach.ngay_thu_hoach,thu_hoach.san_luong,thu_hoach.image as hinh_anh_thu_hoach,thu_hoach.qrcode FROM rau
+            LEFT JOIN thu_hoach ON rau.id = thu_hoach.rau_id WHERE rau.deleted_at = 0"; 
    
     $lists = $db->fetchSql($sql);
     echo json_encode($lists);
--- do_an_tot_nghiep/admin/api/vegetableById.php
+++ do_an_tot_nghiep/admin/api/vegetableById.php
@@ -3,10 +3,16 @@
     require_once  "../autoload/autoload.php";
     
     $id = (int)getInput('id');
+    $productById = $db->fetchID('rau',$id); 
+    $product_thu_hoach = $db->fetchOne('thu_hoach'," deleted_at = 0 AND rau_id = $id ");
 
-    $sql = "SELECT * FROM rau WHERE rau.deleted_at='0' AND rau.id = ".$id;
-      
-    $lists = $db->fetchSql($sql);
-    echo json_encode($lists);
+    // dieu kien canh tac
+    $sql = "SELECT ten_dieu_kien,dieu_kien FROM dieu_kien_canh_tac 
+    WHERE deleted_at = 0 AND rau_id = $id";
+    $data['dieu_kien_canh_tac'] = $db->fetchSql($sql);
+
+
+    $getData = array_merge($productById,$product_thu_hoach,$data);      
+    echo json_encode($getData);
 
 ?>
