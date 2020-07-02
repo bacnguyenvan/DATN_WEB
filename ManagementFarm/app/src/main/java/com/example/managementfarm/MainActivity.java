5e8131064b8a68bdf9484b61a7885967e1f6aaf2
16119 <dangtruongsinh123@gmail.com>
2020-07-02 21:55:48 +0700

    add app admin

---
+++ ManagementFarm/app/src/main/java/com/example/managementfarm/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.managementfarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://smarttrangtrai.com/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+            webView.goBack();
+        super.onBackPressed();
+    }
+}
